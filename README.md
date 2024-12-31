c15d736795e36f68672ea5e30e98ecf073163a22
Samuel Morais <43157951+adsamuelmorais@users.noreply.github.com>
2024-12-31 13:00:15 +0000

    web_crud

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# picpay-web-crud
-Prova Técnica: Desenvolvimento de Aplicação CRUD com Flask
+# Desenvolvimento de Aplicação CRUD com Flask
+
+**Descrição:**
+Este projeto é uma aplicação Flask simples que permite realizar operações CRUD (Criar, Ler, Atualizar, Deletar) em uma entidade de "Usuário". Os dados são persistidos em um banco de dados SQLite.
+
+**Tecnologias:**
+* Python
+* Flask
+* SQLAlchemy
+* SQLite
+
+**Instalação:**
+1. Clone o repositório: `git clone https://github.com/seu_usuario/seu_repositorio.git`
+2. Crie um ambiente virtual: `python -m venv case`
+3. Ative o ambiente virtual: `case\Scripts\activate` (Windows) ou `source case/bin/activate` (Linux/macOS)
+4. Instale as dependências: `pip install -r requirements.txt`
+
+**Execução:**
+```bash
+flask run
+```
+
+**Requisições:**
+
+1. Retorna a lista de todos os usuários.
+```bash
+curl -X GET -H "Content-Type: application/json" http://127.0.0.1:5000/users
+```
+2. Retorna os detalhes de um usuário específico.
+```bash
+curl -X GET -H "Content-Type: application/json" http://127.0.0.1:5000/users/1
+```
+3. Adiciona um novo usuário.
+```bash
+curl -X POST -H "Content-Type: application/json" -d '{"name": "Samuel Morais", "email": "adsamuelmorais@gmail.com", "birth_date": "1987-06-01"}' http://127.0.0.1:5000/users
+```
+4. Atualiza os dados de um usuário existente.
+```bash
+curl -X PUT -H "Content-Type: application/json" -d '{"email": "teste@gmail.com"}' http://127.0.0.1:5000/users/1
+```
+5. Remove um usuário.
+```bash
+curl -X DELETE -H "Content-Type: application/json" http://127.0.0.1:5000/users/1
+```
+
+**Testes**
+
+Para executar testes unitários
+```bash
+pytest
+```
---
+++ tests/test_app.py
@@ -0,0 +1,94 @@
+import os
+import sys
+
+import pytest
+
+# Adicionando o diretório pai para sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from app import User, app, db
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
+        yield client
+        db.session.remove()
+        db.drop_all()
+
+
+def test_create_user(client):
+    data = {
+        'name': 'João Silva',
+        'email': 'joao@email.com',
+        'birth_date': '1987-06-01'
+        }
+    response = client.post('/users', json=data)
+    assert response.status_code == 201
+    assert response.json['message'] == 'Usuário criado com sucesso'
+
+
+def test_create_user_invalid_email(client):
+    data = {
+        'name': 'João Silva',
+        'email': 'invalid_email',
+        'birth_date': '1987-06-01'
+        }
+    response = client.post('/users', json=data)
+    assert response.status_code == 400
+
+
+def test_create_user_invalid_birthdate(client):
+    data = {
+        'name': 'João Silva',
+        'email': 'joao@gmail.com',
+        'birth_date': '1500-06-01'
+        }
+    response = client.post('/users', json=data)
+    assert response.status_code == 400
+
+
+def test_get_user_by_id(client):
+    # Cria um usuário
+    data = {
+        'name': 'João Silva',
+        'email': 'joao@email.com',
+        'birth_date': '1987-06-01'
+        }
+    response = client.post('/users', json=data)
+    assert response.status_code == 201
+
+    user_id = response.json.get("user_id")
+    response = client.get(f'/users/{user_id}')
+    assert response.status_code == 200
+    assert response.json['name'] == 'João Silva'
+
+
+def test_get_user_not_found(client):
+    response = client.get('/users/999')
+    assert response.status_code == 404
+
+
+def test_update_user(client):
+    # Cria um usuário
+    data = {
+        'name': 'João Silva',
+        'email': 'joao@email.com',
+        'birth_date': '1987-06-01'
+        }
+    response = client.post('/users', json=data)
+    assert response.status_code == 201
+
+    user_id = response.json.get("user_id")
+    data = {'name': 'Maria Silva'}
+    response = client.put(f'/users/{user_id}', json=data)
+    assert response.status_code == 200
+
+    updated_user = User.query.get(user_id)
+    assert updated_user.name == 'Maria Silva'
+
+
+def test_delete_nonexistent_user(client):
+    response = client.delete('/users/999')
+    assert response.status_code == 404
